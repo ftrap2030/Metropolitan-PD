07b16fa9865cbf327ce4b97919cd75713a79db4c
ftrap2030 <ftrapaga24@gmail.com>
2026-08-29 13:19:46 -0400

    Route department management prefix commands

--- src/services/leo/prefixCommandsExtended.js
+++ src/services/leo/prefixCommandsExtended.js
@@ -2,15 +2,20 @@ import { isLeoGlobalProtectionCommand, handleLeoGlobalProtectionCommand } from '
 import { isLeoCompatibilityPrefixCommand, handleLeoCompatibilityPrefixCommand } from './prefixCompatibilityCommands.js';
 import { isLeoOwnerPrefixCommand, handleLeoOwnerPrefixCommand } from './prefixOwnerCommands.js';
 import { isLeoAdminPrefixCommand, handleLeoAdminPrefixCommand } from './prefixAdminCommands.js';
+import { isDepartmentPrefixCommand, handleDepartmentPrefixCommand } from './prefixDepartmentCommands.js';
 
 export function isLeoExtendedPrefixCommand(commandName) {
-  return isLeoGlobalProtectionCommand(commandName)
+  return isDepartmentPrefixCommand(commandName)
+    || isLeoGlobalProtectionCommand(commandName)
     || isLeoCompatibilityPrefixCommand(commandName)
     || isLeoOwnerPrefixCommand(commandName)
     || isLeoAdminPrefixCommand(commandName);
 }
 
 export async function handleLeoExtendedPrefixCommand(message, commandName, args, client) {
+  if (isDepartmentPrefixCommand(commandName)) {
+    return handleDepartmentPrefixCommand(message, commandName, args, client);
+  }
   if (isLeoGlobalProtectionCommand(commandName)) {
     return handleLeoGlobalProtectionCommand(message, commandName, args, client);
   }
